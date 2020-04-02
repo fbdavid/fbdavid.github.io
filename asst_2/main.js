e5934a40cae45de8a66d54f371c9b5c61da2b8c3
fdavid <50179226+fbdavid@users.noreply.github.com>
2020-04-02 17:13:39 -0400

    not working

--- asst_2/main.js
+++ asst_2/main.js
@@ -6,8 +6,29 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
+for (let i = 0; i <= 5; i++) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
+newImage.setAttribute('src', 'images/pic' + i + '.jpg');
 thumbBar.appendChild(newImage);
+newImage.onclick = function(fav) {
+displayedImage.src = fav.target.src;
+   }
+}
 
+    
 /* Wiring up the Darken/Lighten button */
+
+btn.onclick = function() {
+    const btnclass = btn.getAttribute('class');
+    if (btnclass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten'; 
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)'; 
+    } else {
+      btn.setAttribute('class','dark'); 
+      btn.textContent = 'Darken';  
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+        
+
+    }
+}
